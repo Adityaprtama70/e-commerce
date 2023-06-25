7870e48fc1d1f5f08546b5a7f01332770800279e
Aditya Wahyu Pratama <adityawahyu131@gmail.com>
2023-06-25 12:51:43 +0700

    Initial Commit

---
+++ E-Commerce App UI/E_Commerce_App_UIApp.swift
@@ -0,0 +1,17 @@
+//
+//  E_Commerce_App_UIApp.swift
+//  E-Commerce App UI
+//
+//  Created by Adit on 25/06/23.
+//
+
+import SwiftUI
+
+@main
+struct E_Commerce_App_UIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
